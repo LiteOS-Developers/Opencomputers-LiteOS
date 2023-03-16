ee987017e5cc27675b8fce0146c04f971106c4ac
thegame4craft <48205247+thegame4craft@users.noreply.github.com>
2023-03-16 21:42:04 +0100

    huge kernel cleanup

--- src/System/Kernel/kernel.lua
+++ src/System/Kernel/kernel.lua
@@ -7,8 +7,10 @@ _G.VERSION_INFO.release = "dev"
 
 k = {screen={}}
 
+local files = component.invoke(computer.getBootAddress(), "list", "/System/Kernel/modules")
+table.sort(files)
 
-for _,file in ipairs(component.invoke(computer.getBootAddress(), "list", "/System/Kernel/modules")) do
+for _,file in ipairs(files) do
     local module, err = _G.lib.loadfile("/System/Kernel/modules/" .. file)
     if not module then
         error(err)
@@ -24,18 +26,49 @@ k.threading.createThread("ProcessDeamon", function()
     end
 end):start()
 
--- k.threading.createThread("shell", function()
-    local shell = k.system.executeFile("/System/Lib/Shell.lua").create("/")
-    --[[shell:createDevice("tty0")
-    shell:mapToTTY()
-    repeat
-        shell:execute("/Bin/shell.lua", {"--shell", "tty0"})
-        coroutine.yield()
-    until false]]
-    -- k.write("OS Running")
--- end):start()
+local function parseError(...)
+    local v = table.pack(...)
+    local ok = v[1]
+    local err = v[2]
+    -- k.write("33: " .. dump(v))
+    
+    if ok then
+        -- k.write("OK")
+        return {err, nil}
+    end
+    return {nil, err}
+end
 
--- error(dump(k.threading.threads))
+local function callSecure(fnc, ...)
+    local ok, err = xpcall(fnc, debug.traceback, ...)
+    return table.unpack(parseError(ok, err))
+end
+
+-- k.write(dump(k.filesystem.listDir("/dev")))
+
+k.threading.createThread("shell", function()
+    local builtins = require("Sandbox").builtins
+    _G.syscall = builtins.syscall
+    _G.ioctl = builtins.ioctl
+
+    local shell = require("Shell")
+    
+    local sh, err
+    local sh, err = callSecure(shell.connect, "tty0")
+    if not sh then
+        error(err)
+    end
+    local result, err = callSecure(sh.execute, sh, "/Bin/shell.lua")
+    k.write(dump(err))
+    if type(result) == nil then
+        k.panic(err)
+    end
+    k.write("Result: " .. dump(result))
+    sh:close()
+
+    _G.syscall, _G.ioctl = nil, nil
+end):start()
+
 -- thread management (look in /System/Kernel/threading.lua)
 while true do
     for thread, v in pairs(k.threading.threads) do
@@ -45,12 +78,24 @@ while true do
             -- _G.write("DEAD: " .. k)
             goto continue
         end
-        result = table.pack(coroutine.resume(v.coro))
+        result = table.pack(pcall(coroutine.resume, v.coro))
+        if not result[1] then
+            table.remove(result, 1)
+            k.panic(table.concat(result, " "))
+        end
+        table.remove(result, 1)
         if result[1] == true and result.n >= 3 then
             if result[2] == "syscall" then
                 local call = result[3]
-                local data = result[4] or {}
-                coroutine.resume(v.coro, table.unpack(k.processSyscall(call, data)))
+                local r = result
+                table.remove(r, 1)
+                table.remove(r, 1)
+                table.remove(r, 1)
+                r.n = nil
+                local data = r
+
+                local result, e = k.processSyscall(call, data)
+                coroutine.resume(v.coro, result, e)
             end
         end
         ::continue::
