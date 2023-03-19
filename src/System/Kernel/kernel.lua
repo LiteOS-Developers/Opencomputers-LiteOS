0f00fe3d554b333f9e328042305027342133e918
thegame4craft <48205247+thegame4craft@users.noreply.github.com>
2023-03-19 17:57:29 +0100

    Made a working kernel with syscall & separtion

--- src/System/Kernel/kernel.lua
+++ src/System/Kernel/kernel.lua
@@ -18,53 +18,49 @@ for _,file in ipairs(files) do
     module()
 end
 
-k.write("Running CoreOS v" .. _G.VERSION_INFO.major .. "." .. _G.VERSION_INFO.minor .. "." .. _G.VERSION_INFO.micro .. "-".. _G.VERSION_INFO.release)
+k.printk(k.L_INFO, "Running CoreOS v" .. _G.VERSION_INFO.major .. "." .. _G.VERSION_INFO.minor .. "." .. _G.VERSION_INFO.micro .. "-".. _G.VERSION_INFO.release)
 
 k.threading.createThread("ProcessDeamon", function()
     while true do
         coroutine.yield()
     end
-end):start()
+end) --:start()
+
 
 local function parseError(...)
     local v = table.pack(...)
     local ok = v[1]
     local err = v[2]
-    -- k.write("33: " .. dump(v))
     
     if ok then
-        -- k.write("OK")
         return {err, nil}
     end
     return {nil, err}
 end
 
-local function callSecure(fnc, ...)
-    local ok, err = xpcall(fnc, debug.traceback, ...)
-    return table.unpack(parseError(ok, err))
-end
-
--- k.write(dump(k.filesystem.listDir("/dev")))
 
 k.threading.createThread("shell", function()
-    local builtins = require("Sandbox").builtins
-    _G.syscall = builtins.syscall
-    _G.ioctl = builtins.ioctl
+
+
+    local sandbox = require("Sandbox")
+    local env = sandbox.create_env()
+    _G.syscall = env.syscall
+    _G.ioctl = env.ioctl
 
     local shell = require("Shell")
     
     local sh, err
-    local sh, err = callSecure(shell.connect, "tty0")
+    _G.filesystem = env.filesystem
+    local sh, err = shell.connect("tty0")
     if not sh then
-        error(err)
-    end
-    local result, err = callSecure(sh.execute, sh, "/Bin/shell.lua")
-    k.write(dump(err))
-    if type(result) == nil then
         k.panic(err)
     end
-    k.write("Result: " .. dump(result))
-    sh:close()
+    local result = sh:execute("/Bin/shell.lua")
+    repeat
+       coroutine.yield()
+    until result.stopped
+    k.write("ExitCode: " .. dump(result.result))
+
 
     _G.syscall, _G.ioctl = nil, nil
 end):start()
@@ -72,36 +68,21 @@ end):start()
 -- thread management (look in /System/Kernel/threading.lua)
 while true do
     for thread, v in pairs(k.threading.threads) do
+        if k.threading.threads[thread].stopped then goto continue end
+        result = table.pack(coroutine.resume(v.coro))
         if coroutine.status(v.coro) == "dead" then
-            -- if k.threading == nil then error(k) end
+            k.threading.threads[thread].result = result[2]
             k.threading.threads[thread]:stop()
-            -- _G.write("DEAD: " .. k)
             goto continue
         end
-        result = table.pack(pcall(coroutine.resume, v.coro))
         if not result[1] then
-            table.remove(result, 1)
-            k.panic(table.concat(result, " "))
+            k.panic(dump(result))
         end
-        table.remove(result, 1)
-        if result[1] == true and result.n >= 3 then
-            if result[2] == "syscall" then
-                local call = result[3]
-                local r = result
-                table.remove(r, 1)
-                table.remove(r, 1)
-                table.remove(r, 1)
-                r.n = nil
-                local data = r
-
-                local result, e = k.processSyscall(call, data)
-                coroutine.resume(v.coro, result, e)
-            end
-        end
+        coroutine.resume(v.coro, table.unpack({k.processSyscall(result)}))
         ::continue::
-    end
-    s = table.pack(computer.pullSignal(0.01))
-    if s.n > 0 then
-        computer.pushSignal(table.unpack(s))
+        s = table.pack(computer.pullSignal(0.01))
+        if s.n > 0 then
+            computer.pushSignal(table.unpack(s))
+        end
     end
 end
