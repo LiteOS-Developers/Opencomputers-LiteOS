d781f62c036ebd0cb839d756ab52c53a4fb435ac
thegame4craft <48205247+thegame4craft@users.noreply.github.com>
2023-05-14 14:26:45 +0200

    mount cli interface

--- src/Bin/mount.lua
+++ src/Bin/mount.lua
@@ -1,8 +1,11 @@
 return {
     main = function(args)
         local lfs = require("System.lfs/drive")
-        lfs.mount("/dev/hd0p1", "/drive")
-        
+        if #args < 3 then
+            print(string.format("%s <device> <target>", args[1]))
+            return -1
+        end
+        lfs.mount(args[2], args[3])
     end
 }
 
