a257668444dfce9661f083918f85a790e8e519c6
thegame4craft <48205247+thegame4craft@users.noreply.github.com>
2023-07-31 14:52:55 +0200

    changed readme.md and scripts changed

--- scripts/build.py
+++ scripts/build.py
@@ -16,10 +16,17 @@ REPOS = [
     "generated", "kinit"
 ]
 
+def build(repo, base):
+    os.environ["SRC"] = os.path.join(base, repo)
+    if 'build.py' in os.listdir(repo):
+        subprocess.call([py, os.path.join(repo, 'build.py')])
+    elif 'build.sh' in os.listdir(repo):
+        subprocess.call([os.path.join(base, repo, 'build.sh')])
+    else:
+        raise Exception("No build file for repo '%s'" % repo)
+
 if __name__ == "__main__":
-    if os.path.isdir("build"):
-        shutil.rmtree("build")
-    os.mkdir("build")
+    
 
     base = os.path.abspath(__file__)
     base = os.path.sep.join(os.path.dirname(base).split(os.path.sep)[:-1])
@@ -27,12 +34,20 @@ if __name__ == "__main__":
     os.environ["TARGET"] = os.path.join(base, "build")
     os.environ["BASE"] = base
 
+    if len(sys.argv) >= 2:
+        if sys.argv[1] in REPOS:
+            repo = sys.argv[1]
+            build(repo, base)
+            sys.exit(0)
+        else:
+            print("No repo '%s' found" % sys.argv[1])
+            sys.exit(1)
+
+    if os.path.isdir("build"):
+        shutil.rmtree("build")
+    os.mkdir("build")
+
 
     for repo in REPOS:
-        os.environ["SRC"] = os.path.join(base, repo)
-        if 'build.py' in os.listdir(repo):
-            subprocess.call([py, os.path.join(repo, 'build.py')])
-        elif 'build.sh' in os.listdir(repo):
-            subprocess.call([os.path.join(base, repo, 'build.sh')])
-        else:
-            raise Exception("No build file for repo '%s'" % repo)
+        build(repo, base)
+        
--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -20,25 +20,29 @@ class PreProcessor:
     
     def process(self):
         skip = False
+        skip_n = 0
         for line in self.infile.readlines():
             if line.endswith("\n"): line = line[:-1]
             if len(line) == 0: continue
+            if skip_n > 0:
+                skip_n -= 1
+                continue
 
             """if result := re.match("--#define([\s]+)([A-Za-z][\w\d]+)([\s]+)([\w\s\d()+\-\*/,]+)", line.strip()):
                 self.defines[result.groups()[1]] = result.groups()[3]
             el"""
-            if result := re.match("--#define([\s]+)([A-Za-z][\w\d]+)", line.strip()):
+            if (result := re.match("--#define([\s]+)([A-Za-z][\w\d]+)", line.strip())) and not skip:
                 self.defines[result.groups()[1]] = True
                 continue
-            if result := re.match(r"--#undef([\s]+)([A-Za-z][\w\d]+)", line.strip()):
+            if (result := re.match(r"--#undef([\s]+)([A-Za-z][\w\d]+)", line.strip())) and not skip:
                 self.defines[result.groups()[1]] = False
-            elif result := re.match(r"--#ifdef([\s]+)([A-Za-z][\w\d]+)", line.strip()):
+            elif (result := re.match(r"--#ifdef([\s]+)([A-Za-z][\w\d]+)", line.strip())) and not skip:
                 if not self.defines.get(result.groups()[1]):
                     skip = True
-            elif result := re.match(r"--#ifndef([\s]+)([A-Za-z][\w\d]+)", line.strip()):
+            elif (result := re.match(r"--#ifndef([\s]+)([A-Za-z][\w\d]+)", line.strip())) and not skip:
                 if self.defines.get(result.groups()[1]):
                     skip = True
-            elif result := re.match(r"--#include([\s]+)\"([\w\/.]+)\"", line):
+            elif (result := re.match(r"--#include([\s]+)\"([\w\/.]+)\"", line.strip())) and not skip:
                 buf = io.StringIO()
 
                 p = self.__class__.__new__(self.__class__)
@@ -47,8 +51,10 @@ class PreProcessor:
                 p.defines = self.defines
                 p.process()
                 print(buf.getvalue(), file=self.outfile)
-            elif result := re.match(r"--#nl", line.strip()):
+            elif result := re.match(r"--#nl", line.strip()) and not skip:
                 print("", file=self.outfile)
+            elif (result := re.match(r"--#skip([\s]+)([\d]+)", line.strip())) and not skip:
+                skip_n = int(result.groups()[1])
             elif result := re.match(r"--#endif", line.strip()):
                 skip = False
             elif not skip:
@@ -62,6 +68,12 @@ if __name__ == "__main__":
         infile = args[0]
         outfile = args[1]
 
+    cwd = os.getcwd()
+
+    base = os.path.dirname(os.path.abspath(infile))
+    
+    os.chdir(base)
     p = PreProcessor(infile, outfile)
     p.process()
     p.outfile.close()
+    os.chdir(cwd)
