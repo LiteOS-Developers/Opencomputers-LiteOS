0277d1634f5225a9b5b4e9dbaddcab994db3d5d9
thegame4craft <48205247+thegame4craft@users.noreply.github.com>
2023-08-13 12:14:51 +0200

    [coreutils] start commands using sh.lua

--- coreutils/src/bin/sh.lua
+++ coreutils/src/bin/sh.lua
@@ -1,6 +1,20 @@
 return {
-    main = function(...)
-        printf("sh.lua %s\n", dump(table.pack(...)))
-        return true
+    main = function(args)
+        args[0] = nil
+        local parsed = {}
+        local i = 1
+        while i <= #args do
+            local a = args[i]
+            if a == "-c" then
+                i = i + 1
+                parsed["command"] = args[i]
+            end
+            i = i + 1
+        end
+        if parsed.command ~= nil then
+            syscall("fork", function()
+                syscall("execve", parsed.command, {})
+            end)
+        end
     end
 }
