adb50cb523451ed4484c4642e46edb270ebfd2ad
thegame4craft <48205247+thegame4craft@users.noreply.github.com>
2023-09-21 16:51:51 +0200

    Build system tweaks

--- scripts/build.py
+++ scripts/build.py
@@ -11,24 +11,27 @@ elif os.name == "nt":
 else:
     assert False
 
+base = os.path.abspath(__file__)
+base = os.path.sep.join(os.path.dirname(base).split(os.path.sep)[:-1])
+
 REPOS = [
     "bootloader", "kernel", "coreutils", "liblua",
     "generated", "kinit"
 ]
+env = lambda repo : [f'TARGET="{base}/{repo}/build"', f'BASE="{base}"', f'SRC="{base}/{repo}"']
 
 def build(repo, base):
-    os.environ["SRC"] = os.path.join(base, repo)
-    if 'build.py' in os.listdir(repo):
-        subprocess.call([py, os.path.join(repo, 'build.py')])
-    elif 'build.sh' in os.listdir(repo):
-        subprocess.call([os.path.join(base, repo, 'build.sh')])
+    subprocess.call(["rm", "-rf", f"{base}/{repo}/build"])
+    print(f"[CLEAN] {repo}")
+    if 'Makefile' in os.listdir(repo):
+        print(f"[BUILD] {repo}")
+        subprocess.call(["make", "--no-print-directory", "-C", repo, "build", *env(repo)])
+        print(f"[INSTALl] {repo}")
+        subprocess.call(["cp", "-a", f"{base}/{repo}/build/.", f"{base}/build"])
     else:
         raise Exception("No build file for repo '%s'" % repo)
 
 if __name__ == "__main__":
-    base = os.path.abspath(__file__)
-    base = os.path.sep.join(os.path.dirname(base).split(os.path.sep)[:-1])
-
     os.environ["TARGET"] = os.path.join(base, "build")
     os.environ["BASE"] = base
 
