84b2b034e82c2ed3f20e7e3321d87e3df98c9b12
thegame4craft <48205247+thegame4craft@users.noreply.github.com>
2023-09-21 16:52:42 +0200

    removed build.sh in kernel

--- kernel/build.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-mkdir -p $TARGET/boot
-
-
-if python3 $BASE/scripts/preprocess.py $SRC/src/main.lua $TARGET/boot/kernel.lua; then
-    printf "[  \033[1;92mOK\033[0;39m  ] Kernel\n"
-else
-    rm $TARGET/boot/kernel.lua
-fi
