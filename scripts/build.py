4bdb3934cbc1bcf2d2ce216801e462f1b90b96a9
thegame4craft <48205247+thegame4craft@users.noreply.github.com>
2023-10-15 11:44:38 +0200

    faster build script

--- scripts/build.py
+++ scripts/build.py
@@ -3,6 +3,7 @@ import sys
 import subprocess
 import shutil
 import pathlib
+import threading as th
 
 if os.name == "posix":
     py = "python3"
@@ -16,7 +17,7 @@ base = os.path.sep.join(os.path.dirname(base).split(os.path.sep)[:-1])
 
 REPOS = [
     "bootloader", "kernel", "coreutils", "liblua",
-    "generated", "kinit"
+    "generated", "kinit", "devtab"
 ]
 env = lambda repo : [f'TARGET="{base}/{repo}/build"', f'BASE="{base}"', f'SRC="{base}/{repo}"']
 
@@ -25,11 +26,24 @@ def build(repo, base):
     print(f"[CLEAN] {repo}")
     if 'Makefile' in os.listdir(repo):
         print(f"[BUILD] {repo}")
-        subprocess.call(["make", "--no-print-directory", "-C", repo, "build", *env(repo)])
-        print(f"[INSTALl] {repo}")
-        subprocess.call(["cp", "-a", f"{base}/{repo}/build/.", f"{base}/build"])
+        subprocess.run(["make", "--no-print-directory", "-C", repo, "build", *env(repo)])
+        print(f"[INSTALL] {repo}")
+        subprocess.run(["cp", "-a", f"{base}/{repo}/build/.", f"{base}/build"])
     else:
         raise Exception("No build file for repo '%s'" % repo)
+
+def buildAll(args):
+    tasks:list[th.Thread] = []
+    for arg in args:
+        if arg in REPOS:
+            thread = th.Thread(target=build, args=(arg, base))
+            thread.start()
+            tasks.append(thread)
+        else:
+            print("No repo '%s' found" % arg)
+            sys.exit(1)
+    _ = [t.join() for t in tasks]
+    sys.exit(0)
 
 if __name__ == "__main__":
     os.environ["TARGET"] = os.path.join(base, "build")
@@ -37,19 +51,11 @@ if __name__ == "__main__":
 
     if len(sys.argv) >= 2:
         args = sys.argv[1:]
-        for arg in args:
-            if arg in REPOS:
-                build(arg, base)
-            else:
-                print("No repo '%s' found" % arg)
-                sys.exit(1)
-        sys.exit(0)
+        buildAll(args)
 
     if os.path.isdir("build"):
         shutil.rmtree("build")
     os.mkdir("build")
-
-
-    for repo in REPOS:
-        build(repo, base)
+    
+    buildAll(REPOS)
         
