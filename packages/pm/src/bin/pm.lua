fffbfcab556e81a6d2936561620829186799b172
thegame4craft <48205247+thegame4craft@users.noreply.github.com>
2023-12-30 17:25:21 +0100

    first try to make a package manager

---
+++ packages/pm/src/bin/pm.lua
@@ -0,0 +1,312 @@
+local pm = require("pm")
+local shell = require("shell")
+local filesystem = require("filesystem")
+local tar require("tar")
+local io = require("io")
+local pm = require("pm")
+
+local f = string.format
+local opts
+
+local RM_BLACKLIST = {
+    ["/bin/"] = true,
+    ["/boot/"] = true,
+    ["/dev/"] = true,
+    ["/etc/"] = true,
+    ["/home/"] = true,
+    ["/lib/"] = true,
+    ["/mnt/"] = true,
+    ["/tmp/"] = true,
+    ["/usr/"] = true,
+    ["/usr/lib/"] = true,
+    ["/usr/bin/"] = true,
+}
+
+local os = {
+    exit = function(code)
+        syscall("exit", code)
+    end
+}
+
+local function printHelp()
+    printf("pm [opts] <mode> [args]")
+    printf("mode :")
+    printf("\tinstall <packageFile>")
+    printf("\tuninstall <packageName>")
+    printf("\tinfo <packageName>|<packageFile>")
+    printf("\tlist-installed")
+    printf("opts :")
+    printf("\t--purge : remove configuration files")
+    printf("\t--dry-run : do not really perform the operation")
+    printf("\t--include-removed : also list non completly removed packages")
+    printf("\t--allow-same-version : allow a package of the same version to be used for to update the installed version")
+end
+
+local function extractPackage(packagePath)
+    checkArg(1, packagePath, "string")
+
+    if (opts["dry-run"]) then return true, nil end
+    packagePath = filesystem.canonical(packagePath)
+    printf("Extracting : %s", packagePath)
+    return tar.extract(packagePath, "/", false, "DATA/", nil, "DATA/")
+end
+
+local function rm(...)
+    local cmd = f("rm %s", table.concat({...}, " "))
+    if (opts["dry-run"]) then
+        print(cmd)
+    else
+        filesystem.remove(table.concat({...}, " "))
+    end
+end
+
+local rmdir = rm
+
+local function compareVersion(a, b)
+    local aMajor, aMinor, aPatch = a:match("(%d+)%.(%d+)%.(%d+)")
+    local bMajor, bMinor, bPatch = b:match("(%d+)%.(%d+)%.(%d+)")
+    if (aMajor > bMajor) then return true elseif (aMajor < bMajor) then return false end
+    if (aMinor > bMinor) then return true elseif (aMinor < bMinor) then return false end
+    if (aPatch > bPatch) then return true elseif (aPatch < bPatch) then return false end
+    return true --equal
+end
+
+local nativePrintf = printf
+function printf(str)
+    nativePrintf(str .. "\n")
+end
+
+local print = printf
+ 
+return {
+    main = function(args) 
+        local mode = table.remove(args, 1)
+        local args, opts = shell.parse(args)
+        -- printHelp()
+        if (mode == "info") then
+            local manifest
+            if (args[1]:match("%.tar$")) then
+                args[1] = shell.resolve(args[1])
+                local r, rr
+                manifest, r, rr = pm.getManifestFromPackage(args[1])
+                if (not manifest) then
+                    printf("Error : %s, %s", r, rr or "")
+                    os.exit(1)
+                end
+            else
+                if (pm.isInstalled(args[1])) then
+                    manifest = pm.getManifestFromInstalled(args[1])
+                else
+                    printf("Could not find package named %q", args[1])
+                    os.exit(1)
+                end
+            end
+        
+            printf("Name : %s (%s)", manifest.name, manifest.package)
+            printf("Version : %s", manifest.version)
+            if (manifest.description) then printf("Description : \n%s", manifest.description) end
+            if (manifest.note) then printf("Note :\n%s", manifest.note) end
+            if (manifest.authors) then printf("Authors : %s", manifest.authors) end
+            if (manifest.dependencies) then
+                print("Dependencies :")
+                for name, version in pairs(manifest.dependencies) do
+                    printf("%s (%s)", name, version)
+                end
+            end
+            os.exit(0)
+        elseif (mode == "list-installed") then
+            for name, manifest in pairs(pm.getInstalled(opts["include-removed"])) do
+                if (opts["include-removed"]) then
+                    local installed, removed = pm.isInstalled(name)
+                    local label = "installed"
+                    if (not installed and removed) then label = "config remaining" end
+                    printf("%s (%s) [%s]", name, manifest.version, label)
+                else
+                    printf("%s (%s)", name, manifest.version)
+                end
+            end
+        elseif (mode == "install") then
+            --get the pacakge file absolute path
+            args[1] = shell.resolve(args[1])
+        
+            --get the manifest from the package
+            local manifest, r, rr = pm.getManifestFromPackage(args[1])
+            if (not manifest) then
+                printf("Invalid package")
+                os.exit(1)
+            end
+        
+            --check if the package is already installed
+            if (pm.isInstalled(manifest.package)) then
+                local currentManifest = pm.getManifestFromInstalled(manifest.package)
+                if (manifest.version == "oppm" or currentManifest.version == "oppm") then
+                    --do nothing. We force reinstallation for oppm since there is no version number
+                elseif (manifest.version == currentManifest.version and not opts["allow-same-version"]) then
+                    printf("Package %q is already installed", manifest.package)
+                    os.exit(0)
+                elseif (compareVersion(currentManifest.version, manifest.version) and not opts["allow-same-version"]) then --downgrade
+                    printf("Cannot downgrade package %q", manifest.package)
+                    os.exit(1)
+                end
+            end
+        
+            --check the pacakge's dependencies
+            if (manifest.dependencies) then
+                for dep, version in pairs(manifest.dependencies) do
+                    if (not pm.isInstalled(dep)) then
+                        printf("Missing dependencie : %s (%s)", dep, version)
+                        os.exit(1)
+                    end
+                    local compType = version:match("^[<>=]") or ">"
+                    version = version:match("%d.*$")
+                    local installedVersion = pm.getManifestFromInstalled(dep).version
+                    if (version) then
+                        if (installedVersion == "oppm") then
+                            printf("Warning : %s is using a oppm version. Cannot determine real installed version", dep)
+                        else
+                            if (compType == "=") then
+                                if (not version == installedVersion) then
+                                    printf("Package %s require %s version %s but %s is installed", manifest.package, dep, version, installedVersion)
+                                    os.exit(1)
+                                end
+                            elseif (compType == "<") then
+                                if (compareVersion(version, installedVersion)) then
+                                    printf("Package %s require %s version %s but %s is installed", manifest.package, dep, version, installedVersion)
+                                    os.exit(1)
+                                end
+                            else
+                                if (compareVersion(version, installedVersion) and version ~= installedVersion) then
+                                    printf("Package %s require %s version %s but %s is installed", manifest.package, dep, version, installedVersion)
+                                    os.exit(1)
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+        
+            --make the values the keys for easier test later
+            local configFiles = {}
+            if (manifest.configFiles) then
+                for _, file in pairs(manifest.configFiles) do
+                    configFiles[file] = true
+                end
+            end
+        
+            local installedFiles = {}
+            if (pm.isInstalled(manifest.package)) then
+                local installedFileListFile = assert(io.open(f("/etc/pm/info/%s.files", manifest.package)))
+                for file in installedFileListFile:lines() do
+                    installedFiles[file] = true
+                end
+                installedFileListFile:close()
+            end
+        
+            --check that no file not from the package get overwriten
+            for _, header in pairs(assert(tar.list(args[1]))) do
+                if (header.name:match("^DATA/") and header.typeflag == "file") then
+                    local destination = header.name:sub(#("DATA/"))
+                    if (not installedFiles[destination] and (filesystem.exists(destination) and not configFiles[destination])) then
+                        printf("\27[37mFile already exists %s\27[m", destination)
+                        os.exit(1)
+                    end
+                end
+            end
+        
+            if (pm.isInstalled(manifest.package)) then
+                local currentManifest = pm.getManifestFromInstalled(manifest.package)
+                printf("Installing %s (%s) over %s (%s)", manifest.package, manifest.version, currentManifest.package, currentManifest.version)
+            else
+                printf("Installing : %s (%s)", manifest.package, manifest.version)
+            end
+        
+            --uninstall old version. It's easier than to check wich file need to be deleted
+            if (pm.isInstalled(manifest.package)) then
+                --print("Unistalling currently installed version")
+                shell.execute(f("pm uninstall %q --no-dependencies-check", manifest.package))
+            end
+        
+            --extract the files in the correct path
+            local extracted, reason = extractPackage(args[1])
+            if (not extracted) then
+                printf("\27[37m%s\27[m", reason or "Unkown error")
+                --TODO : revert installation
+                os.exit(1)
+            end
+        
+            --save the package info and file list
+            if (not opts["dry-run"]) then
+                filesystem.makeDirectory("/etc/pm/info/")
+                local listFile = assert(io.open(f("/etc/pm/info/%s.files", manifest.package), "w"))
+                for _, header in pairs(assert(tar.list(args[1]))) do
+                    if (header.name:match("^DATA")) then
+                        local name = header.name:match("DATA(.*)")
+                        if (name and name ~= "") then listFile:write(name .. "\n") end
+                    end
+                end
+                listFile:close()
+                assert(tar.extract(args[1], "/tmp/pm/", true, "CONTROL/manifest", nil, "CONTROL/"))
+                filesystem.rename("/tmp/pm/manifest", f("/etc/pm/info/%s.manifest", manifest.package))
+            end
+        
+            --remove the tmp folder
+            --cleanup(extracted)
+        elseif (mode == "uninstall") then
+            --check if the package exists
+            if (not pm.isInstalled(args[1])) then
+                printf("Package %q is not installed", args[1])
+                os.exit(0)
+            end
+        
+            local manifest = pm.getManifestFromInstalled(args[1])
+        
+            --check dep
+        
+            if (not opts["no-dependencies-check"]) then
+                local cantUninstall, dep = pm.checkDependant(args[1])
+                if (cantUninstall) then
+                    printf("Cannot uninstall %s. One or more package (%s) depend on it.", args[1], dep)
+                    os.exit(1)
+                end
+            end
+        
+            printf("Uninstalling : %s", args[1])
+        
+            --make the values the keys for easier test later
+            local configFiles = {}
+            if (manifest.configFiles) then
+                for _, file in pairs(manifest.configFiles) do
+                    configFiles[file] = true
+                end
+            end
+        
+            local fileListFile = assert(io.open(f("/etc/pm/info/%s.files", args[1])))
+            local dirs = {}
+            --delete the files
+            for path in fileListFile:lines() do
+                if (not configFiles[path] or opts.purge) then
+                    if (not filesystem.isDirectory(path)) then
+                        rm(f("%s", path))
+                    else
+                        if (path and path ~= "/" and path ~= "") then
+                            table.insert(dirs, 1, path)
+                        end
+                    end
+                end
+            end
+            fileListFile:close()
+            --delete empty directory left behind
+            for _, dir in pairs(dirs) do
+                if (not RM_BLACKLIST[dir]) then
+                    rmdir(f("%s", dir))
+                end
+            end
+            rm(f("%s", f("/etc/pm/info/%s.files", args[1])))
+            if (opts.purge) then rm(f("%s", f("/etc/pm/info/%s.manifest", args[1]))) end
+        else
+            printHelp()
+            os.exit(0)
+        end --]]
+    end
+
+}
---
+++ packages/pm/src/lib/tar.lua
@@ -0,0 +1,364 @@
+local filesystem = require("filesystem")
+local io = require("io")
+
+---@class tarHeader
+---@field name any
+---@field mode any
+---@field uid any
+---@field gid any
+---@field size any
+---@field mtime any
+---@field chksum any
+---@field typeflag any
+---@field linkname any
+---@field magic any
+---@field version any
+---@field uname any
+---@field gname any
+---@field devmajor any
+---@field devminor any
+---@field prefix any
+
+---https://github.com/luarocks/luarocks/blob/master/src/luarocks/core/dir.lua
+local function unquote(c)
+    local first, last = c:sub(1, 1), c:sub(-1)
+    if (first == '"' and last == '"') or
+        (first == "'" and last == "'") then
+        return c:sub(2, -2)
+    end
+    return c
+end
+local dir = {}
+function dir.path(...)
+    local t = {...}
+    while t[1] == "" do
+        table.remove(t, 1)
+    end
+    for i, c in ipairs(t) do
+        t[i] = unquote(c)
+    end
+    return (table.concat(t, "/"):gsub("([^:])/+", "%1/"):gsub("^/+", "/"):gsub("/*$", ""))
+end
+
+---https://github.com/luarocks/luarocks/blob/master/src/luarocks/tools/tar.lua#L56
+--- A pure-Lua implementation of untar (unpacking .tar archives)
+local tar = {}
+
+local blocksize = 512
+
+local function get_typeflag(flag)
+    if flag == "0" or flag == "\0" then
+        return "file"
+    elseif flag == "1" then
+        return "link"
+    elseif flag == "2" then
+        return "symlink" -- "reserved" in POSIX, "symlink" in GNU
+    elseif flag == "3" then
+        return "character"
+    elseif flag == "4" then
+        return "block"
+    elseif flag == "5" then
+        return "directory"
+    elseif flag == "6" then
+        return "fifo"
+    elseif flag == "7" then
+        return "contiguous" -- "reserved" in POSIX, "contiguous" in GNU
+    elseif flag == "x" then
+        return "next file"
+    elseif flag == "g" then
+        return "global extended header"
+    elseif flag == "L" then
+        return "long name"
+    elseif flag == "K" then
+        return "long link name"
+    end
+    return "unknown"
+end
+
+local function octal_to_number(octal)
+    local exp = 0
+    local number = 0
+    octal = octal:gsub("%s", "")
+    for i = #octal, 1, -1 do
+        local digit = tonumber(octal:sub(i, i))
+        if not digit then
+            break
+        end
+        number = number + (digit * 8 ^ exp)
+        exp = exp + 1
+    end
+    return number
+end
+
+local function checksum_header(block)
+    local sum = 256
+    for i = 1, 148 do
+        local b = block:byte(i) or 0
+        sum = sum + b
+    end
+    for i = 157, 500 do
+        local b = block:byte(i) or 0
+        sum = sum + b
+    end
+    return sum
+end
+
+local function nullterm(s)
+    return s:match("^[^%z]*")
+end
+
+local function read_header_block(block)
+    local header = {}
+    header.name = nullterm(block:sub(1, 100))
+    header.mode = nullterm(block:sub(101, 108)):gsub(" ", "")
+    header.uid = octal_to_number(nullterm(block:sub(109, 116)))
+    header.gid = octal_to_number(nullterm(block:sub(117, 124)))
+    header.size = octal_to_number(nullterm(block:sub(125, 136)))
+    header.mtime = octal_to_number(nullterm(block:sub(137, 148)))
+    header.chksum = octal_to_number(nullterm(block:sub(149, 156)))
+    header.typeflag = get_typeflag(block:sub(157, 157))
+    header.linkname = nullterm(block:sub(158, 257))
+    header.magic = block:sub(258, 263)
+    header.version = block:sub(264, 265)
+    header.uname = nullterm(block:sub(266, 297))
+    header.gname = nullterm(block:sub(298, 329))
+    header.devmajor = octal_to_number(nullterm(block:sub(330, 337)))
+    header.devminor = octal_to_number(nullterm(block:sub(338, 345)))
+    header.prefix = block:sub(346, 500)
+    if checksum_header(block) ~= header.chksum then
+        return false, "Failed header checksum"
+    end
+    return header
+end
+
+
+function tar.untar(filename, destdir)
+    checkArg(1, filename, "string")
+    checkArg(2, destdir, "string")
+
+    local tar_handle = io.open(filename, "rb")
+    if not tar_handle then return nil, "Error opening file " .. filename end
+
+    local long_name, long_link_name
+    local ok, err
+    local make_dir = filesystem.makeDirectory
+    while true do
+        local block
+        repeat
+            block = tar_handle:read(blocksize)
+        until (not block) or checksum_header(block) > 256
+        if not block then break end
+        if #block < blocksize then
+            ok, err = nil, "Invalid block size -- corrupted file?"
+            break
+        end
+        local header
+        header, err = read_header_block(block)
+        if not header then
+            ok = false
+            break
+        end
+
+        local file_data = tar_handle:read(math.ceil(header.size / blocksize) * blocksize):sub(1, header.size)
+
+        if header.typeflag == "long name" then
+            long_name = nullterm(file_data)
+        elseif header.typeflag == "long link name" then
+            long_link_name = nullterm(file_data)
+        else
+            if long_name then
+                header.name = long_name
+                long_name = nil
+            end
+            if long_link_name then
+                header.name = long_link_name
+                long_link_name = nil
+            end
+        end
+        local pathname = dir.path(destdir, header.name)
+        pathname = filesystem.abspath(pathname)
+        if header.typeflag == "directory" then
+            make_dir(pathname)
+        elseif header.typeflag == "file" then
+            local dirname = filesystem.abspath(pathname)
+            if dirname ~= "" then
+                make_dir(dirname)
+            end
+            local file_handle
+            file_handle, err = io.open(pathname, "wb")
+            if not file_handle then
+                ok = nil
+                require("event").onError("[tar]" .. err)
+                break
+            end
+            file_handle:write(file_data):close()
+        end
+    end
+    tar_handle:close()
+    return ok, err
+end
+
+--END OF luarocks original code
+
+---Retrun the headers
+---@param filename string
+---@return table<number,tarHeader>?, string? reason
+function tar.list(filename)
+    checkArg(1, filename, 'string')
+    local tar_handle = io.open(filename, "rb")
+    if not tar_handle then return nil, "Error opening file " .. filename end
+    local ok, err
+    local long_name, long_link_name
+    local files = {}
+    while true do
+        local block
+        repeat
+            block = tar_handle:read(blocksize)
+        until (not block) or checksum_header(block) > 256
+        if not block then break end
+        if #block < blocksize then
+            ok, err = nil, "Invalid block size -- corrupted file?"
+            break
+        end
+        local header
+        header, err = read_header_block(block)
+        if not header then
+            ok = false
+            break
+        end
+        local file_data = tar_handle:read(math.ceil(header.size / blocksize) * blocksize):sub(1, header.size)
+        if header.typeflag == "long name" then
+            long_name = nullterm(file_data)
+        elseif header.typeflag == "long link name" then
+            long_link_name = nullterm(file_data)
+        else
+            if long_name then
+                header.name = long_name
+                long_name = nil
+            end
+            if long_link_name then
+                header.name = long_link_name
+                long_link_name = nil
+            end
+        end
+        if (header.typeflag == "file" or header.typeflag == "directory") then
+            table.insert(files, header)
+        end
+    end
+    tar_handle:close()
+    return files, err
+end
+
+---Extract files that match filename `fineNameInArchive:match("^"..filename)
+---@param tarname string
+---@param filename? string|table
+---@param overwrite boolean overwrite existing files.
+---@param ignore? string|table
+---@param destdir string
+---@param newRoot? string
+function tar.extract(tarname, destdir, overwrite, filename, ignore, newRoot)
+    checkArg(1, tarname, "string")
+    checkArg(2, destdir, "string")
+    checkArg(3, overwrite, "boolean")
+    checkArg(4, filename, "string", "table", 'nil')
+    checkArg(5, ignore, "string", "table", 'nil')
+    checkArg(6, destdir, "string")
+    checkArg(7, newRoot, "string", "nil")
+
+    ---@param name string
+    ---@return boolean
+    local function shouldExtractFile(name)
+        local ok = true
+        if (filename and type(filename) == "string") then
+            if (not name:match("^" .. filename)) then ok = false end
+        end
+        if (filename and type(filename) == "table") then
+            for _, fname in pairs(filename) do
+                if (not name:match("^" .. fname)) then ok = false end
+            end
+        end
+        if (ignore and type(ignore) == "string") then
+            if (name:match("^" .. ignore)) then ok = false end
+        end
+        if (ignore and type(ignore) == "table") then
+            for _, fname in pairs(ignore) do
+                if (name:match("^" .. fname)) then ok = false end
+            end
+        end
+        return ok
+    end
+
+    local tar_handle = io.open(tarname, "rb")
+    if not tar_handle then return nil, "Error opening file " .. tarname end
+
+    local long_name, long_link_name
+    ---@type boolean?, string?
+    local ok, err = true, nil
+    local make_dir = filesystem.makeDirectory
+    while true do
+        local block
+        repeat
+            block = tar_handle:read(blocksize)
+        until (not block) or checksum_header(block) > 256
+        if not block then break end
+        if #block < blocksize then
+            ok, err = nil, "Invalid block size -- corrupted file?"
+            break
+        end
+        local header
+        header, err = read_header_block(block)
+        if not header then
+            ok = false
+            break
+        end
+
+        local file_data = tar_handle:read(math.ceil(header.size / blocksize) * blocksize):sub(1, header.size)
+
+        if header.typeflag == "long name" then
+            long_name = nullterm(file_data)
+        elseif header.typeflag == "long link name" then
+            long_link_name = nullterm(file_data)
+        else
+            if long_name then
+                header.name = long_name
+                long_name = nil
+            end
+            if long_link_name then
+                header.name = long_link_name
+                long_link_name = nil
+            end
+        end
+
+
+        if (shouldExtractFile(header.name)) then
+            local pathname = dir.path(destdir, header.name)
+            if (newRoot) then --change the archive root
+                pathname = pathname:gsub(newRoot, "")
+            end
+            pathname = filesystem.abspath(pathname)
+
+            if header.typeflag == "directory" then
+                make_dir(pathname)
+            elseif header.typeflag == "file" then
+                local dirname = filesystem.dirpath(pathname)
+                if dirname ~= "" then
+                    make_dir(dirname)
+                end
+                if (overwrite or not (filesystem.exists(pathname) and not filesystem.isDirectory(pathname))) then
+                    local file_handle
+                    file_handle, err = io.open(pathname, "wb")
+                    if not file_handle then
+                        ok = nil
+                        require("event").onError("[tar]" .. err)
+                        break
+                    end
+                    file_handle:write(file_data):close()
+                end
+            end
+        end
+    end
+    tar_handle:close()
+    return ok, err
+end
+
+return tar
